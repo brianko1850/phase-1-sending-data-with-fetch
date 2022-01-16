6ea12a598f4524bc0573b3a7122f0b8f5724e6ba
brianko <kobj11@gmail.com>
2022-01-16 11:43:03 -0500

    commit tree 77b0bbbbacdb557edb5976e42c1407d760b4e12b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+
+
+  let p = document.createElement("p")
+  document.body.appendChild(p)
+function showData(obj) {
+   p.textContent = obj
+}
+
+ function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email
+  };
+    return fetch('http://localhost:3000/users',{
+         method: 'POST',
+         headers: {
+             'Content-Type': 'application/json',
+             'accept': 'application/json'
+         },
+         body: JSON.stringify(formData)
+        })
+        .then(res => res.json())
+        .then(data => showData(data.id))
+        .catch(error => showData(error))
+      }
